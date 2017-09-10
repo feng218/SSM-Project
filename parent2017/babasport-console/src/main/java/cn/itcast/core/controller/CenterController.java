0286050df60bc0204091a3b73e2d3f4f317fa3aa
yufeng218 <he1012315243@163.com>
2017-09-10 22:28:35 +0800

    描述： （1）后台页面的搭建；添加jsp、css、js、image等静态文件； （2）首页的显示；

--- parent2017/babasport-console/src/main/java/cn/itcast/core/controller/CenterController.java
+++ parent2017/babasport-console/src/main/java/cn/itcast/core/controller/CenterController.java
@@ -10,24 +10,79 @@ import cn.itcast.core.bean.TestTb;
 import cn.itcast.core.service.TestTbService;
 
 @Controller
+@RequestMapping(value = "/control")
 public class CenterController
 {
-	@Autowired
-	private TestTbService testTbService;
+	//入口
+	@RequestMapping(value = "/index.do")
+	public String index()
+	{
+		return "index";
+	}
+	
+	//头
+	@RequestMapping(value = "/top.do")
+	public String top()
+	{
+		return "top";
+	}
+	
+	//身体
+	@RequestMapping(value = "/main.do")
+	public String main()
+	{
+		return "main";
+	}
+	
+	//左边
+	@RequestMapping(value = "/left.do")
+	public String left()
+	{
+		return "left";
+	}
+	
+	//右边
+	@RequestMapping(value = "/right.do")
+	public String right()
+	{
+		return "right";
+	}
+	
+	//商品的身体
+	@RequestMapping(value = "/frame/product_main.do")
+	public String product_main()
+	{
+		return "frame/product_main";
+	}
+	
+	//商品身体左
+	@RequestMapping(value = "/frame/product_left.do")
+	public String product_left()
+	{
+		return "frame/product_left";
+	}
+	
+	
+	
+	
+	
+	
+//	@Autowired
+//	private TestTbService testTbService;
 	
 	//	返回值的三种形式:
 	//		ModelView: Model 和 View 放在一起, 因为需要解耦的关系所以不使用;
 	//		void	: 使用原生的request, ajax异步时使用;
 	//		String	: 返回一个视图, 例如return "index"; ajax同步时使用; model.addAttribute();
 	//		测试
-	@RequestMapping(value = "/control/index.do")
-	public String index()
-	{
-		TestTb testTb = new TestTb();
-		testTb.setName("controller1");
-		testTb.setBirthday(new Date());
-		testTbService.insertTestTb(testTb);
-		return "index";
-	}
+//	@RequestMapping(value = "/control/index.do")
+//	public String index()
+//	{
+//		TestTb testTb = new TestTb();
+//		testTb.setName("controller1");
+//		testTb.setBirthday(new Date());
+//		testTbService.insertTestTb(testTb);
+//		return "index";
+//	}
 	
 }
