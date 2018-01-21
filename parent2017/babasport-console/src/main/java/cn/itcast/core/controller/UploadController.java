294fb76f4d24dd777be3749aecab082afef0bf5e
yufeng218 <he1012315243@163.com>
2018-01-21 15:32:13 +0800

    多图片上传

--- parent2017/babasport-console/src/main/java/cn/itcast/core/controller/UploadController.java
+++ parent2017/babasport-console/src/main/java/cn/itcast/core/controller/UploadController.java
@@ -1,5 +1,9 @@
 package cn.itcast.core.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
@@ -7,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import cn.itcast.common.web.Constants;
@@ -20,7 +25,7 @@ public class UploadController
 	
 	//上传单张图片
 	@RequestMapping(value = "/upload/uploadPic.do")
-	public void toEdit(@RequestParam(required = false) MultipartFile pic,
+	public void uploadPic(@RequestParam(required = false) MultipartFile pic,
 			HttpServletResponse response)
 	{
 		System.out.println(pic.getOriginalFilename());
@@ -45,5 +50,32 @@ public class UploadController
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	@RequestMapping(value = "/upload/uploadPics.do")
+	@ResponseBody
+	public List<String> uploadPics(@RequestParam(required = false) MultipartFile[] pics) throws IOException
+	{
+		List<String> list = new ArrayList<String>(); //保存上传图片后的URL
+		for (MultipartFile pic : pics)
+		{
+			System.out.println(pic.getOriginalFilename());
+			try
+			{
+				String name = pic.getOriginalFilename(); //文件名
+				long size = pic.getSize(); //文件大小
+				
+				// group1/M00/00/00/wKisFFpBG9eAHaQvAAAWKd1hQR4158_big.jpg
+				String path = uploadService.uploadPic(pic.getBytes(), name, size);
+				String url = Constants.IMAGE_URL + path; //全路径
+				list.add(url);
+				
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		return list;
 	}
 }
